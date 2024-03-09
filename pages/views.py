09a262d8b372be53b1e959dedb6050abdfd07dac
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-03-09 18:11:38 +0700

    template inheritance and context

--- pages/views.py
+++ pages/views.py
@@ -9,11 +9,19 @@ def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "home.html", {})
 
+
 def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
+
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123, 456, 789],
+    }
+    return render(request, "about.html", my_context)
+
 
 def social_view(request, *args, **kwargs):
     return render(request, "social.html", {})
