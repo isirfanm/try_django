a7d3a5c1097c3de79cae09f27a694f0c07323bcb
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-03-09 17:12:10 +0700

    pages

--- try_django/settings.py
+++ try_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # 3rd parties
 
     # own
+    'pages',
     'products',
 ]
 
--- try_django/urls.py
+++ try_django/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view, contact_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home_view, name="home"),
+    path("contact/", contact_view),
+    path("admin/", admin.site.urls),
 ]
