4bf19825662e145ccbc42075219820e3b1e0d4e6
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-03-09 16:39:11 +0700

    more change on model

--- products/models.py
+++ products/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120)  # max_length required
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(decimal_places=2, max_digits=18) 
-    summary = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=18)
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField()
