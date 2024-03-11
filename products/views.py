4bcbbfbe8b28a8d26d78fbb8a3203332ee32c90e
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-03-11 20:59:42 +0700

    create product using model form

--- products/views.py
+++ products/views.py
@@ -1,11 +1,23 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 from .models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
+def product_create_view(request: HttpRequest):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {"form": form}
+    return render(request, "products/product_create.html", context)
+
+
 def product_detail_view(request):
     product = Product.objects.get(id=1)
     # context = {"title": product.title, "description": product.description}
     context = {"product": product}
-    return render(request, "product/detail.html", context)
+    return render(request, "products/product_detail.html", context)
