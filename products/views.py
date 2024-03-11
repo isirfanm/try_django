6bf64b307e045f6edcbf3b3d78b41bca9b4ddcd7
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-03-11 20:22:33 +0700

    pass product object to template

--- products/views.py
+++ products/views.py
@@ -6,5 +6,6 @@ from .models import Product
 # Create your views here.
 def product_detail_view(request):
     product = Product.objects.get(id=1)
-    context = {"title": product.title, "description": product.description}
+    # context = {"title": product.title, "description": product.description}
+    context = {"product": product}
     return render(request, "product/detail.html", context)
