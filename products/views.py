b7b4659c34733ad12ab31c6cee28d063c1c05db2
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-04-12 23:21:36 +0700

    raw form

--- products/views.py
+++ products/views.py
@@ -7,13 +7,27 @@ from .forms import ProductForm
 
 # Create your views here.
 def product_create_view(request: HttpRequest):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
-    context = {"form": form}
+    context = {}
     return render(request, "products/product_create.html", context)
+
+
+# def product_create_view(request: HttpRequest):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == "POST":
+#         my_new_title = request.POST.get("title")
+#         print(my_new_title)
+#     context = {}
+#     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request: HttpRequest):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {"form": form}
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
