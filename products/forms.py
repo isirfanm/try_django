d7ac616262c66257a84fcb8449a4e70deaa4b8e8
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-04-12 23:55:31 +0700

    form widgets

--- products/forms.py
+++ products/forms.py
@@ -7,3 +7,22 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["title", "description", "price"]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(
+        label="", widget=forms.TextInput(attrs={"placeholder": "Your title"})
+    )
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your description",
+                "id": "my-id-for-textarea",
+                "class": "new-class-name two",
+                "rows": 20,
+                "cols": 120,
+            }
+        ),
+    )
+    price = forms.DecimalField(initial=199.99)
--- products/views.py
+++ products/views.py
@@ -2,12 +2,23 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 
 # Create your views here.
 def product_create_view(request: HttpRequest):
-    context = {}
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        "form": my_form
+    }
     return render(request, "products/product_create.html", context)
 
 
