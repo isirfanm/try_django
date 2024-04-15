bfc59088033fe40d03476d9bafcda7c9d96e721c
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-04-15 22:24:34 +0700

    raw details

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from .views import CourseView
 
 urlpatterns = [
     path("", CourseView.as_view(template_name="courses/course_about.html"), name="course-list"),
+    path("<int:id>/", CourseView.as_view(), name="course-detail"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+from .models import Course
 
 # Create your views here.
 
 
 # Based View Class = View
 class CourseView(View):
-    template_name = "courses/about.html"
+    template_name = "courses/course_detail.html"
 
     # get method for GET request
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+    def get(self, request, id=None, *args, **kwargs):
+        context = {}
+        if id:
+            object = get_object_or_404(Course, id=id)
+            context["object"] = object
+        return render(request, self.template_name, context)
 
     # post method for POST request
     def post(self, request, *args, **kwargs):
