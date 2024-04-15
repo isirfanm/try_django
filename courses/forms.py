c10bcf8152e631939d3eae3cbefbf449eb4f869d
Mohamad Irfan Islami <is.irfan2m@gmail.com>
2024-04-15 22:50:54 +0700

    raw validation

--- courses/forms.py
+++ courses/forms.py
@@ -6,3 +6,9 @@ class CourseModelForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ["title"]
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if title.lower() == "abc":
+            raise forms.ValidationError("This is not a valid title")
+        return title
